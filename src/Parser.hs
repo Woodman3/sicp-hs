9228aaaef869983c4c762af60bed12e72132e4d4
Woodman3 <102576074@qq.com>
2024-12-27 17:51:55 +0800

    add error message for parser

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,28 +1,73 @@
+module Parser where
 
-data Parser a = Parser(String->Maybe(a,String))
+import Control.Applicative
+import Data.List (nub)
 
-parse ::Parser a -> String -> Maybe(a,String)
-parse (Parser par) str = par str
+type Offset = Int
 
-item :: (Char->Bool)->Parser Char
-item f = Parser (\str-> case str of
-    (x:xs)-> if f x then Just(x,xs) else Nothing
-    []-> Nothing)
+data Error i e = Error {
+    error_pos :: Offset,
+    error_type :: ErrorType i e
+} deriving (Show,Eq)
 
-char :: Char->Parser Char
-char c =  item (\x -> x == c)
+data ErrorType i e 
+    = UnexpectedEndOfInput
+    | Expect i i
+    | ExpectEOF i
+    | Unexpected i
+    | CustomError e
+    | Empty
+    deriving (Show,Eq)
 
-string :: String -> Parser String
-string "" = Parser(\str -> Just("",str))
-string (x:xs) = Parser(\str -> 
-    case parse (char x) str of
-        Just(y,ys) -> case parse (string xs) ys of
-            Just(z,zs) -> Just(x:xs,zs)
-            Nothing -> Nothing
-        Nothing -> Nothing )
+data Parser i e a = Parser{ parse :: [i]->Offset->Either [ Error i e ] (a,Offset,[i]) } 
 
-bind :: Parser a -> (a-> Parser b) -> Parser b
-pa `bind` f = Parser(\str->
-    case parse pa str of
-        Just(a,xs)-> f a
-        Nothing -> Nothing)
+error_token ::(i->ErrorType i e) -> (i->Bool)->Parser i e i
+error_token mk_err f = Parser $ \input offset -> case input of
+    (x:xs)-> if f x then Right (x,offset+1,xs) else Left [Error offset ( mk_err x )] 
+    []-> Left [Error offset UnexpectedEndOfInput]
+
+satisfy :: (i->Bool)->Parser i e i 
+satisfy f = error_token (\x -> Unexpected x) f
+
+eof :: Parser i e ()
+eof = Parser $ \input offset -> case input of
+    []-> Right ((),offset,[])
+    (x:xs)-> Left [Error offset ( ExpectEOF x )]
+
+char :: Eq i => i ->Parser i e i 
+char c = error_token (\x -> Expect c x) (==c)
+
+string :: Eq i => [i] -> Parser i e [i] 
+string [] = return []
+string (x:xs) = do
+    rx <- char x
+    rxs <- string xs
+    return (rx:rxs)
+
+instance Functor ( Parser i e )  where
+    fmap f pa = Parser $ \input offset  -> do
+        (x,offset',xs) <- parse pa input offset
+        return (f x,offset',xs) 
+    -- fmap f pa = Parser $ \a-> case parse pa a of 
+    --     Just(x,xs)-> Just(f x,xs)
+    --     Nothing -> Nothing
+
+instance Applicative (Parser i e) where
+    pure x = Parser $ \input offset-> Right (x,offset,input) 
+    pf <*> pa = Parser $ \input offset -> do
+        (f,offset',input') <- parse pf input offset
+        (a,offset'',input'') <- parse pa input' offset'
+        return (f a,offset'',input'')
+
+instance Monad (Parser i e) where
+    pa >>= f = Parser $ \input offset -> do
+        (a,offset',rest) <- parse pa input offset
+        parse (f a) rest offset'
+
+instance ( Eq i , Eq e ) => Alternative (Parser i e) where
+    empty = Parser $ \_ offset -> Left [Error offset Empty] 
+    pa <|> pb = Parser $ \input offset-> case parse pa input offset of
+        Right r  -> Right r
+        Left e -> case parse pb input offset of
+            Right r  -> Right r
+            Left e' -> Left $ nub $ e <> e' 
--- src/Token.hs
+++ src/Token.hs
@@ -2,12 +2,27 @@ module Token(Token(..),
     Op(..),
     is_op) where
 
+import Parser
+
 data Token = Int Int | Op Op | LPar | RPar
     deriving (Show,Eq)
 
 data Op = Mul | Sub | Add | Div
-    deriving (Show,Eq)
+    deriving (Eq)
+
+instance Show Op where
+    show Mul = "*"
+    show Sub = "-"
+    show Add = "+"
+    show Div = "/"
 
 is_op :: Token -> Bool
 is_op (Op _) = True
 is_op _ = False
+
+-- digit :: Parser Int
+-- digit = read <$> some (satisfy isDigit)
+
+-- op :: Parser Op
+-- op = read <$> satisfy $ \c-> c == '*' || c == '/' || c == '+' || c == '-'
+
